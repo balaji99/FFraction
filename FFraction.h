d10d9e136e3c0e535d45786e9cd19020e5e38598
balaji99 <balaji99@users.noreply.github.com>
2016-08-10 00:57:36 +0530

    Create FFraction.h

---
+++ FFraction.h
@@ -0,0 +1,37 @@
+#include <vector>
+
+using namespace std;
+
+class FFraction
+{
+private:
+	vector<long long> n, d;
+
+public:
+	/* ToDo: Implement the following
+	FFraction();
+	FFraction(FFraction &);
+
+	FFraction operator = (double);
+	FFraction operator = (long long);
+	FFraction operator = (FFraction &);
+
+	FFraction operator * (double);
+	FFraction operator * (long long);
+	FFraction operator * (FFraction &);
+	
+	FFraction operator / (double);
+	FFraction operator / (long long);
+	FFraction operator / (FFraction &);
+	*/
+
+	/*
+	ToDo: Define and implement the following
+	overloaded operators: /=, *=, ==, +, +=, -, -=
+	destructor
+	reduceNumer: multiplies all factors in the numerator and stores the product as the only factor
+	reduceDenom: multiplies all factors in the denominator and stores the product as the only factor
+	reduce: removes common factors between numerator and denominator. i.e. ensures numerator and denominator are coprime.
+	reduceToSingleNum: divides the numerator by the denominator to produce a double-precision floating-point number
+	*/
+};
